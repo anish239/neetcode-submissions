db23feb0d849f305e9ccbac743e300246c9cd2b3
anish239 <161726096+anish239@users.noreply.github.com>
2026-05-27 18:37:58 +0530

    Add: house-robber-ii - submission-0

---
+++ Data Structures & Algorithms/house-robber-ii/submission-0.cpp
@@ -0,0 +1,22 @@
+class Solution {
+   public:
+    int robRange(vector<int>& nums, int start, int end) {
+        int prev2 = 0, prev1 = 0;
+        for (int i = start; i <= end; i++) {
+            int curr = max(prev1, prev2 + nums[i]);
+            prev2 = prev1;
+            prev1 = curr;
+        }
+        return prev1;
+    }
+
+    int rob(vector<int>& nums) {
+        int n = nums.size();
+        if (n == 1) return nums[0];
+
+        int range1 = robRange(nums, 0, n - 2);  
+        int range2 = robRange(nums, 1, n - 1);  
+
+        return max(range1, range2);
+    }
+};
