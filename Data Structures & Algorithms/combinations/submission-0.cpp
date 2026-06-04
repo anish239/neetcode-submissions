7da8dde54955c4a75499a84000d8fb99062efd56
anish239 <161726096+anish239@users.noreply.github.com>
2026-06-04 13:00:51 +0530

    Add: combinations - submission-0

---
+++ Data Structures & Algorithms/combinations/submission-0.cpp
@@ -0,0 +1,22 @@
+class Solution {
+    private:
+    void solve(int start, int n, int k, vector<int>&curr, vector<vector<int>>&ans){
+        int m = curr.size();
+        if(m == k){
+            ans.push_back(curr);
+            return;
+        }
+        for(int i = start; i<=n; i++){
+            curr.push_back(i);
+            solve(i+1,n,k,curr,ans);
+            curr.pop_back();
+        }
+    }
+public:
+    vector<vector<int>> combine(int n, int k) {
+        vector<vector<int>>ans;
+        vector<int>curr;
+        solve(1,n,k,curr,ans);
+        return ans;
+    }
+};
